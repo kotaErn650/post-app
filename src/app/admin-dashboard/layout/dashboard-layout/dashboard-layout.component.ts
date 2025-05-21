ae18fe28db2cfb6e1d420afe558d70179e517df9
joseLopez <kota012011@gmail.com>
2025-05-21 00:22:17 -0500

    Grabando

--- src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
+++ src/app/admin-dashboard/layout/dashboard-layout/dashboard-layout.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-layout.component.html',
   styleUrl: './dashboard-layout.component.css'
 })
