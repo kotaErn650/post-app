bdaeeebaca6ccb18ebf4a8563bac17d5e0d3166b
joseLopez <kota012011@gmail.com>
2025-05-23 14:44:12 -0500

    User Service

--- src/app/admin-dashboard/pages/user-page/user-page.component.ts
+++ src/app/admin-dashboard/pages/user-page/user-page.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserTableComponent } from '../../../users/componets/user-table/user-table.component';
+import { UserService } from '../../../users/services/user.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-user-page',
   imports: [UserTableComponent],
   templateUrl: './user-page.component.html',
-  styleUrl: './user-page.component.css'
+  styleUrl: './user-page.component.css',
 })
 export class UserPageComponent {
+  userService = inject(UserService);
 
+  userResource = rxResource({
+    request: ()=>({}),
+    loader:  ()=>{
+      return this.userService.getUsers()
+    }
+  })
 }
